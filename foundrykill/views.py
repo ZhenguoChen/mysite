a7af14941c035a48665eb78c5dcbb518d1f93d17
ZhenguoChen <zhenguo@clinc.com>
2018-10-18 18:23:51 -0400

    first edition

--- foundrykill/views.py
+++ foundrykill/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+import pandas
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello')
+    with open('foundrykill/data.json', 'r') as csv_file:
+        user_data = pandas.read_csv(csv_file, delimiter=',').T.to_dict().values()
+        context = {
+            'users': user_data
+        }
+    return render(request, 'foundrykill/index.html', context)
